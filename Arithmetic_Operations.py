ca67514780d7e91f46e15df87da5cf29a1c9dea3
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-07-31 01:06:43 +0530

    Arithmetic Operations Practice and implemantation

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,25 @@
+# Task 2: Write a program to perform different Arithmetic Operations on numbers in Python. 
+
+
+# Value 1
+a=10
+# Value 2
+b=5
+# Arithmetic Operations
+
+
+# + Operator
+print(a+b)
+# - Operator
+print(a-b)
+# * Operator
+print(a*b)
+# / Operator
+print(a/b)  # Division by zero will raise a ZeroDivisionError
+# % Operator
+print(a%b)  # Remainder of the division of a by b
+# ** Operator
+print(a**b)  # a raised to the power of b
+# // Operator
+print(a//b)  # Integer division of a by b, i.e., the quotient of
+# a and b rounded down to the nearest whole number.
