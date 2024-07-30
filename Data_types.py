45229852412483b870f06252034cfc80f349390e
ARUN KUAMR <stockerunknown@gmail.com>
2024-07-30 20:12:11 +0530

    uploading first code

---
+++ Data_types.py
@@ -0,0 +1,39 @@
+# Data types in Python
+# 1. Numberic Data types
+# .Integer.
+a=10
+# Float
+b=10.5
+# Complexnumber
+real=10
+imaginary=20
+c=complex(real,imaginary)
+
+
+# 2. Dictonary
+d={"a":"apple","b":"banana"}
+# 3. Bollean
+e=True
+# 4. Set
+f={1,2,3,4,5}
+#  5. Sequence type
+
+# String
+g='hello'
+g1="hello"
+g2="""hello
+        heloo
+        hello
+        heloo
+    """
+# List
+h=[1,"Heloo",7,10,"I am Arun"]
+# Tuple
+i=(5,3,2,4,2,)
+
+
+print(type(a),a,"\n",
+      type(b),b,"\n",
+      type(c),c,"\n",
+      type(d),d,"\n",
+      type(e),e,"\n",type(f),f,"\n",type(g),g,"\n",type(g1),g1,"\n",type(g2),g2,"\n",type(h),h,"\n",type(i),i,"\n")
