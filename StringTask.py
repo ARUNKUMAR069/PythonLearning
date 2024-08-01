0da59f6eaec0b8267779677b0f88b6adc1d6b743
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-01 13:53:42 +0530

    Task based on String Data types
    In this task String is created , Concatenated, Sub-String

---
+++ StringTask.py
@@ -0,0 +1,16 @@
+# Task 3: Write a program to create, concatenate and print a string and accessing sub-stringfrom a given string. 
+
+# CREATING STRING
+str1="STRING1"
+print("First String Created =",str1)
+str2="STRING2"
+print("Second String Created =",str2)
+# CONCATENATING STRINGS
+str3=str1+str2
+# PRINTING CONCATENATED STRING
+print("Concatinating of Strings =",str3)
+#GENERATING A SUB STRING FROM ONE STRING
+substr=str3[1:3]
+print("Creating a substring from Concatenated String =", substr)
+
+
