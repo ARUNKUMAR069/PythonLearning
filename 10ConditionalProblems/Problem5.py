f9f844449f89af29f78ac233c17b33f1eda812f1
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-21 00:09:59 +0530

    Solution of 5 Problem

---
+++ 10ConditionalProblems/Problem5.py
@@ -0,0 +1,12 @@
+# Problem: Suggest an activity based on the weather (e.g., Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman).
+
+print("Hi i am helping to suggest a activity according to weather")
+weather = input("What is the weather like today? = ")
+if ("Sunny"or"sunny") ==weather:
+    print("Go for a walk or have a picnic")
+elif ("Rainy"or"rainy") == weather:
+    print("Read a book or have a movie night")
+elif ("Snowy"or"snowy") == weather:
+    print("Build a snowman or go skiing")
+else:
+    print("I am not sure what to suggest")  # Add a default message if the weather
